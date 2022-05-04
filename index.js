122a62d9023afb5a2f081f998daf06bfdb5f93b4
DianaReviako <81633798+DianaReviako@users.noreply.github.com>
2022-05-04 17:21:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var reporter = require('cucumber-html-reporter');
 var options = {
 
         theme: 'bootstrap',
-        jsonFile: 'reports/report.json',
-        output: 'reports/cucumber_report.html',
-        screenshotsDirectory: 'reports/photo',
+        jsonFile: 'report.json',
+        output: 'cucumber_report.html',
+        screenshotsDirectory: 'photo',
         storeScreenshots: true,
         reportSuiteAsScenarios: true,
         scenarioTimestamp: true,
@@ -13,7 +13,7 @@ var options = {
         metadata: {
             "Title": "Sample Report",
             "Browser": "Chrome",
-            "Platform": "Windows 10"
+            "Platform": "Ubuntu 22.04"
         }
     };
 
