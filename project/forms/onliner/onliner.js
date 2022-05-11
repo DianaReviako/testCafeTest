98ed1761e2ce3fdf48496d3f022572169c51f8b1
DianaReviako <81633798+DianaReviako@users.noreply.github.com>
2022-05-11 18:57:00 +0300

    Update onliner.js

--- project/forms/onliner/onliner.js
+++ project/forms/onliner/onliner.js
@@ -27,6 +27,7 @@ class Onliner extends BaseForm{
     }
 
     async isCheckboxSelected(name){
+        console.log('Hello')
         const checkboxName = name.toLowerCase()
         return this.filterChechbox(checkboxName).isChecked();
     }
